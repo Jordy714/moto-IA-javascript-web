2bdba99792c29582d06a1226433cd0655872e19b
JMleafarM <105086118+Jordy714@users.noreply.github.com>
2024-05-05 15:31:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ carroCanvas.width = 400;
 const networkCanvas = document.getElementById("networkCanvas");
 networkCanvas.width = 400;
 
+let idAnimacion;
 
 const carroCtx = carroCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const carretera = new Carretera(carroCanvas.width / 2, carroCanvas.width * 0.9);
 
-const N = 100;
-const carros = generarCarros(N)
+
+let N = 1;
+let carros = generarCarros(N)
 let mejorCarro = carros[0];
 
 if (localStorage.getItem("mejorCerebro")) {
@@ -24,28 +26,53 @@ if (localStorage.getItem("mejorCerebro")) {
 
 }
 
-const trafico = [
-    new Carro(carretera.lineaCentral(3), -200, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(1), -400, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(2), -200, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(0), -400, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(5), -300, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(3), -900, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(4), -800, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(1), -1000, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(5), -400, 30, 50, "Jordy", 2),
-    new Carro(carretera.lineaCentral(5), -1100, 30, 50, "Jordy", 2)
+let trafico = [
+    new Moto(carretera.lineaCentral(3), -200, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(1), -400, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(2), -200, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(0), -400, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(5), -300, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(3), -900, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(4), -800, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(1), -1000, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(5), -400, 30, 50, "Jordy", 2),
+    new Moto(carretera.lineaCentral(5), -1100, 30, 50, "Jordy", 2)
 ]
-
 animar();
 
+function iniciar() {
+    var nCarros = document.getElementById("nCarros").value;
+    carros = generarCarros(nCarros);
+    trafico = [
+        new Moto(carretera.lineaCentral(3), -200, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(1), -400, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(2), -200, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(0), -400, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(5), -300, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(3), -900, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(4), -800, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(1), -1000, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(5), -400, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(5), -1100, 30, 50, "Jordy", 2),
+        new Moto(carretera.lineaCentral(4), -1100, 30, 50, "Jordy", 2)
+    ]
+    mejorCarro = carros[0];
 
+    if (localStorage.getItem("mejorCerebro")) {
+        for (let i = 0; i < carros.length; i++) {
+            carros[i].cerebro = JSON.parse(
+                localStorage.getItem("mejorCerebro"));
+            if (i != 0) {
+                RedNeuronal.mutate(carros[i].cerebro, 0.1)
+            }
+        }
 
+    }
+}
 function save() {
     localStorage.setItem("mejorCerebro",
         JSON.stringify(mejorCarro.cerebro));
 }
-
 function discard() {
     localStorage.removeItem("mejorCerebro")
 }
@@ -53,13 +80,14 @@ function discard() {
 function generarCarros(N) {
     const carros = [];
     for (let i = 1; i <= N; i++) {
-        carros.push(new Carro(carretera.lineaCentral(3), 100, 30, 50, "IA"));
+        carros.push(new Moto(carretera.lineaCentral(3), 100, 30, 50, "IA"));
 
     }
     return carros;
 }
 
 function animar(time) {
+
     for (let i = 0; i < trafico.length; i++) {
         trafico[i].update(carretera.bordes, []);
     }
@@ -101,5 +129,4 @@ function animar(time) {
     //da la ilucion de moverse
     requestAnimationFrame(animar);
 }
-
     
